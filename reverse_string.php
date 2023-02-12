5c12dadd610c9102df0641c82c79738c68306400
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-02-12 20:05:16 +0800

    Update

--- reverse_string.php
+++ reverse_string.php
@@ -1,18 +1,7 @@
 <?php
 
-function revStr($name2)
-{
-    $reverse_string = "";
-    for ($i = strlen($name2) - 1; $i >= 0; $i--) {
-        $reverse_string .= $name2[$i];
-    }
-    return $reverse_string;
+$reverse_string = "";
+for ($i = strlen($name) - 1; $i >= 0; $i--) {
+    $reverse_string .= $name[$i];
 }
-echo revStr("Abdul Fahad A. Pangandaman");
-
-// Imperative
-// $reverse_string = "";
-// for ($i = strlen($name) - 1; $i >= 0; $i--) {
-//     $reverse_string .= $name[$i];
-// }
-// echo $reverse_string;
+echo $reverse_string;

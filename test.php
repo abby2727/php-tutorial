4b6a08fcd8448960edb24d039caa449c0f2cb151
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-02-16 21:37:08 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hi';
+$array = ['Robert', 'John', 'Ben', 'Cynthia'];
+
+foreach ($array as $index => $dataArray) {
+    // echo "$index \n";
+    // echo "$dataArray \n";
+    echo "$index : $dataArray \n";
+}
